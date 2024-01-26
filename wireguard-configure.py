d387a70061faef8db1a9ebd909c22c226f3a168f
PebPeb <67885698+PebPeb@users.noreply.github.com>
2024-01-25 22:06:19 -0500

    addPeer, Reconfigured to one wgDevice and printing as host or peer

--- wireguard-configure.py
+++ wireguard-configure.py
@@ -6,115 +6,27 @@ import re
 WG_IPv4NETWORK = '10.0.0.0/24'
 
 
-def parse_wireguard_config(file_path):
-  config = {
-    'Interface': {},
-    'Peers': []
-  }
+def main():
+  config_path = './server-conf/wg0-example.conf'  # Replace with your actual file path
 
-  with open(file_path, 'r') as file:
-    current_section = None
+  n = wgConfig(config_path)
+  n.peerConf(n.add_peer())
 
-    devices = []
-    current_device = []
-    first_found = True
-    for line in file:
-      line = line.strip()
-
-      if not line or line.startswith('#'):                # Ignore Comments
-        continue
-      if line.startswith('[') and line.endswith(']'):     # Find Device
-        if not first_found:                               # After found start Adding new finished Devices
-          devices.append(current_device)
-          current_device = []
-        else:                                             # Can't add first Device because does not have lines yet
-          first_found = False
-        current_section = line[1:-1]
-        current_device.append(line)
-        continue
-      if not first_found:                                 # Wait until first device found
-        current_device.append(line)
-    devices.append(current_device)                        # Add trailing Device to list
-
-    for x in devices:
-      if x[0] == "[Interface]":
-        config["Interface"].update(parse_device(x))
-      elif x[0] == "[Peer]":
-        config["Peers"].append(parse_device(x))
-      else:
-        print("WARNING Invalid device \"" + x[1] + "\". Skipping device.")
-
-  return config
-
-def parse_device(device):
-  config = {}
-  for x in device[1:]:      # Skip the first value
-    config.update(__parse_key_pair(x))
-  return config
-
-def __parse_key_pair(text):
-  try:
-    key, value = map(str.strip, text.split('=', 1))
-    return {key: value}
-  except:
-    print("WARNING Unable to parse \"" + text + "\" not a valid key pair. Skipping line.")
-    return None
-
-
-
-def print_config(config):
-
-  print("Interface:")
-  for key, value in config["Interface"].items():
-      print(f"  {key}: {value}")
-  print()
-
-  for peer in config['Peers']:
-      print("Peer:")
-      for key, value in peer.items():
-          print(f"  {key}: {value}")
-      print()
-
-
-
-def new_private_key():
-  return subprocess.run(['wg', 'genkey'], capture_output=True, text=True, check=True).stdout.strip()
-
-def gen_public_key(PrivateKey):
-  return subprocess.run(['wg', 'pubkey'], input=PrivateKey, capture_output=True, text=True, check=True).stdout.strip()
-
-def add_peer(config):
-  # Collecting all the IPs used in the conf so far
-  taken_IPs = [re.sub(r'/\d+$', '', config["Interface"]["Address"])]
-  for x in config["Peers"]:
-    taken_IPs.append(re.sub(r'/\d+$', '',x["AllowedIPs"]))
-
-  # Select new IP 
-
-  subnet = ipaddress.IPv4Network(WG_IPv4NETWORK) # This is currently hardcoded
-  available_ips = [str(ip) for ip in subnet.hosts() if str(ip) not in taken_IPs]
-
-  print(subnet)
-  if available_ips:
-    selected_ip = available_ips[0]
-    print("Selected next available IP address:", selected_ip)
-  else:
-    print("No available IP addresses in the subnet.")
-    exit(-1)
+  # n.serverConf()
   
-  private_key = new_private_key()
 
 
 class wgConfig:
   def __init__(self, configFile=None) -> None:
     self.peers = []
-    self.interface = None
+    self.host = None
+
+    self.peerDir = ""
+
     if configFile != None:
       self.parseConf(configFile)
 
   def parseConf(self, configFile):
-    config = []
-
     devices = []
     current_device = []
     first_found = True
@@ -135,24 +47,24 @@ class wgConfig:
         if not first_found:                                 # Wait until first device found
           current_device.append(line)
       devices.append(current_device)                        # Add trailing Device to list
-      print(devices)
+
       for x in devices:
         if x[0] == "[Interface]":
-          interfacetmp = self.parse_device(x)
-          self.interface = interface(re.sub(r'/\d+$', '', interfacetmp["Address"]), 
-                            ipaddress.IPv4Network(interfacetmp["Address"], strict=False).netmask,
-                            privateKey=interfacetmp["PrivateKey"],
-                            port=interfacetmp["ListenPort"])
+          hosttmp = self.parse_device(x)
+          self.host = wgDevice(re.sub(r'/\d+$', '', hosttmp["Address"]), 
+                            ipaddress.IPv4Network(hosttmp["Address"], strict=False).netmask,
+                            privateKey=hosttmp["PrivateKey"],
+                            port=hosttmp["ListenPort"])
           # config["Interface"].update(parse_device(x))
         elif x[0] == "[Peer]":
           peertmp = self.parse_device(x)
-          self.peers.append(peer(re.sub(r'/\d+$', '', peertmp["AllowedIPs"]), 
+          self.peers.append(wgDevice(re.sub(r'/\d+$', '', peertmp["AllowedIPs"]), 
                             ipaddress.IPv4Network(peertmp["AllowedIPs"], strict=False).netmask,
                             publicKey=peertmp["PublicKey"]))
 
         else:
           print("WARNING Invalid device \"" + x[1] + "\". Skipping device.")
-    
+  
   def parse_device(self, device):
     config = {}
     for x in device[1:]:      # Skip the first value
@@ -167,66 +79,107 @@ class wgConfig:
       print("WARNING Unable to parse \"" + text + "\" not a valid key pair. Skipping line.")
       return None
 
+  def add_peer(self):
+    # Collecting all the IPs used in the conf so far
+    taken_IPs = [self.host.ip]
+    for x in self.peers:
+      taken_IPs.append(x.ip)
+
+    # Select new IP 
+    subnet = ipaddress.IPv4Network(WG_IPv4NETWORK) # This is currently hardcoded
+    available_ips = [str(ip) for ip in subnet.hosts() if str(ip) not in taken_IPs]
+
+    if available_ips:
+      peerIP = available_ips[0]
+      # print("Selected next available IP address:", selected_ip)
+    else:
+      print("No available IP addresses in the subnet.")
+      exit(-1)
+    
+    peerPrivateKey = gen_publicKey()
+    peerPublicKey = gen_privateKey(peerPrivateKey)
+
+    newPeer = wgDevice(peerIP, "255.255.255.0", 
+           privateKey=peerPrivateKey,
+           publicKey=peerPublicKey)
+    self.peers.append(newPeer)
+    return newPeer
+  
+  # Expects to receive a wgDevice object
+  # Directory to write the config file to
+  def peerConf(self, wgObj, writeTo=None):
+    x = wgObj('interface') + "\n"
+    x += self.host('peer')
+    if writeTo != None:
+      try:
+        with open(writeTo, 'w') as file:
+          file.write(x)
+      except:
+        print("Failed to write to: \"" + writeTo + "\"")
+    else:
+      try:
+        with open(self.peerDir + wgObj.name + ".conf", 'w') as file:
+          file.write(x)
+      except:
+        print("Failed to write to: \"" + self.peerDir + wgObj.name + ".conf" + "\"")
+      
+    return x
+
+  def serverConf(self):
+    print(self.host('interface'))
+    for x in self.peers:
+      print(x('peer'))
+
+    
+
+
 class wgDevice:
   def __init__(self, ip, subnet, privateKey=None, publicKey=None, description=None, name=None, port="5180"):
-    self.privateKey = privateKey if privateKey or publicKey else new_private_key()
-    self.publicKey = publicKey if publicKey else gen_public_key(self.privateKey)
+    self.privateKey = privateKey if privateKey or publicKey else gen_publicKey()
+    self.publicKey = publicKey if publicKey else gen_privateKey(self.privateKey)
     self.ip = ip
     self.subnet = subnet
     self.description = description if description else ""
     self.name = name if name else "peer_" + self.ip.replace(".", "_")
     self.port = port
 
-  def __str__(self) -> str:
-    x = "Name = " + self.name + "\n"
-    x = x + "Description = " + self.description + "\n"
-    return x
+  def __str__(self, format_type='default') -> str:
+      if format_type == 'peer' and hasattr(self, 'publicKey'):
+          # [Peer] format
+          x = "[Peer]\n"
+          x += self.__str__()
+          x += "PublicKey = " + self.publicKey + "\n"
+          x += "AllowedIPs = " + self.ip + "/32\n"
+      elif format_type == 'interface' and hasattr(self, 'privateKey'):
+          # [Interface] format
+          x = "[Interface]\n"
+          x += self.__str__()
+          x += "PrivateKey = " + self.privateKey + "\n"
+          x += "Address = " + self.ip + "/24\n"
+          x += "ListenPort = " + str(self.port) + "\n"
+      else:
+          # Default format
+          x = "Name = " + self.name + "\n"
+          x += "Description = " + self.description + "\n"
 
-class peer(wgDevice):
-  def __init__(self, ip, subnet, privateKey=None, publicKey=None, description=None, name=None, port="5180"):
-    super().__init__(ip, subnet, privateKey, publicKey, description, name, port)
+      return x
 
-  def __str__(self) -> str:
-    x = "[Peer]\n"
-    x = x + super().__str__()
-    x = x + "PublicKey = " + self.publicKey + "\n"
-    x = x + "AllowedIPs = " + self.ip + "/32\n"               # Currently the subnet mask is hard coded
-    return x
+  def __call__(self, format_type='default') -> str:
+      return self.__str__(format_type)
 
-class interface(wgDevice):
-  def __init__(self, ip, subnet, privateKey=None, publicKey=None, description=None, name=None, port="5180"):
-    super().__init__(ip, subnet, privateKey, publicKey, description, name, port)
 
-  def __str__(self) -> str:
-    x = "[Interface]\n"
-    x = x + super().__str__()
-    x = x + "PrivateKey = " + self.privateKey + "\n"
-    x = x + "Address = " + self.ip + "/24\n"                  # Currently the subnet mask is hard coded
-    x = x + "ListenPort = " + self.port + "\n"              
-    return x
+def gen_publicKey():
+  return subprocess.run(['wg', 'genkey'], capture_output=True, text=True, check=True).stdout.strip()
+
+def gen_privateKey(PrivateKey):
+  return subprocess.run(['wg', 'pubkey'], input=PrivateKey, capture_output=True, text=True, check=True).stdout.strip()
+
+
+
+
 
 
 
 if __name__ == "__main__":
-  config_path = './server-conf/wg0-example.conf'  # Replace with your actual file path
-  parsed_config = parse_wireguard_config(config_path)
+  main()
 
-  print_config(parsed_config)
-
-  private = new_private_key()
-  print(private)
-  public = gen_public_key(private)
-  print(public)
-
-  print(gen_public_key(parsed_config["Interface"]["PrivateKey"]))
-  add_peer(parsed_config)
-
-
-  test_peer = peer("10.9.9.3", "255.255.255.0")
-
-  print(test_peer)
-
-  n = wgConfig(config_path)
-  print(n.interface)
-  for x in n.peers:
-    print(x)
